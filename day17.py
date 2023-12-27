3e2d5dce64c10a5d2090f1f25fdb7159e6e56a12
gzhu725 <gloriazhu66@gmail.com>
2023-12-26 19:56:21 -0500

    day 17

---
+++ day17.py
@@ -0,0 +1,63 @@
+#creds to hyper neutrino 
+from heapq import heappush, heappop
+
+grid = [list(map(int, line.strip())) for line in open('input17.txt')]
+
+seen = set()
+pq = [(0, 0, 0, 0, 0, 0)]
+def part1(): 
+  while pq:
+      hl, r, c, dr, dc, n = heappop(pq)
+      
+      if r == len(grid) - 1 and c == len(grid[0]) - 1:
+          print(hl)
+          break
+
+      if (r, c, dr, dc, n) in seen:
+          continue
+
+      seen.add((r, c, dr, dc, n))
+      
+      if n < 3 and (dr, dc) != (0, 0):
+          nr = r + dr
+          nc = c + dc
+          if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+              heappush(pq, (hl + grid[nr][nc], nr, nc, dr, dc, n + 1))
+
+      for ndr, ndc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+          if (ndr, ndc) != (dr, dc) and (ndr, ndc) != (-dr, -dc):
+              nr = r + ndr
+              nc = c + ndc
+              if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                  heappush(pq, (hl + grid[nr][nc], nr, nc, ndr, ndc, 1))
+
+#part 2 is just a couple of condition changes
+
+def part2(): 
+  while pq:
+      hl, r, c, dr, dc, n = heappop(pq)
+      
+      if r == len(grid) - 1 and c == len(grid[0]) - 1 and n >= 4:
+          print(hl)
+          break
+
+      if (r, c, dr, dc, n) in seen:
+          continue
+
+      seen.add((r, c, dr, dc, n))
+      
+      if n < 10 and (dr, dc) != (0, 0):
+          nr = r + dr
+          nc = c + dc
+          if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+              heappush(pq, (hl + grid[nr][nc], nr, nc, dr, dc, n + 1))
+      if n >= 4 or (dr, dc) == (0,0):
+        for ndr, ndc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            if (ndr, ndc) != (dr, dc) and (ndr, ndc) != (-dr, -dc):
+                nr = r + ndr
+                nc = c + ndc
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                    heappush(pq, (hl + grid[nr][nc], nr, nc, ndr, ndc, 1))
+
+#part1()
+part2()
